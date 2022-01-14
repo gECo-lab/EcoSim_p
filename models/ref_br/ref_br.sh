20d358b5b803abe918082e1a88bd29ee90c6c8c4
Sergio L M Rivero <sergiolmrivero@gmail.com>
2022-01-14 15:12:37 -0300

    Updating Ref_br model

--- models/ref_br/ref_br.sh
+++ models/ref_br/ref_br.sh
@@ -6,7 +6,7 @@ python3 main.py ../models/ref_br/ config.json model.json scenarios.json
 
 cd ../models/ref_br
 
-#Rscript -e 'rmarkdown::render("ref_br.rmd", output_format="html_document", output_dir="results")'
+Rscript -e 'rmarkdown::render("ref_br.rmd", output_format="html_document", output_dir="results")'
 
 # firefox results/ref_br.html&
 # sensible-browser results/ref_br.html&

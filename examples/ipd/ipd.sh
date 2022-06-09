f2da072c547d3b8a19881261e1d6a30a9b774d3b
Sergio Rivero <sergiolmrivero@gmail.com>
2022-06-09 14:03:03 -0300

    Changings on interface

--- examples/ipd/ipd.sh
+++ examples/ipd/ipd.sh
@@ -6,7 +6,7 @@ python3 main.py ../examples/ipd/ config.json model.json scenarios.json
 
 cd ../examples/ipd
 
-#Rscript -e 'rmarkdown::render("ipd.rmd", output_format="html_document", output_dir="results")'
+Rscript -e 'rmarkdown::render("ipd.rmd", output_format="html_document", output_dir="results")'
 
 #firefox results/ipd.html&
-# sensible-browser results/ipd.html&
+sensible-browser results/ipd.html&

3e585e7f4fd8e1605f775e0d37a8e68e413e2be2
Sergio Rivero <sergiolmrivero@gmail.com>
2024-01-13 13:56:49 -0300

    Econ model - including rmd file

--- examples/benchmark/benchmark.sh
+++ examples/benchmark/benchmark.sh
@@ -1,11 +1,13 @@
 
+
+rm runs/*.csv
+
 cd ../..
 
 python3 ecosimp.py examples/benchmark/ config.json model.json scenarios.json
 
 cd examples/benchmark
 
-#Rscript -e 'rmarkdown::render("macro_model.rmd", output_format="html_document", output_dir="results")'
+Rscript -e 'rmarkdown::render("benchmark.Rmd", output_format="html_document", output_dir="results")'
 
-#firefox results/macro_Caiani.html
-#sensible-browser results/macro_model.html&
+sensible-browser results/benchmark.html&

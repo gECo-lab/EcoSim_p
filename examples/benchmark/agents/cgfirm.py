1d57c05aba35fe1edce4ccbc33f67ee3ab087cce
Sergio Rivero <sergiolmrivero@gmail.com>
2024-10-11 17:52:21 -0300

    Updating firm equations

--- examples/benchmark/agents/cgfirm.py
+++ examples/benchmark/agents/cgfirm.py
@@ -1,4 +1,4 @@
- # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 """ Consumer Goods Firm Agent from the basic macroeconomic model 
 
 
@@ -32,26 +32,23 @@ from .goods import CapitalGood, ConsumptionGood, Labor
 import random as rnd
 
 
-
 class CGFirm(Firm):
-    """ Consumers Goods Firm """
+    """Consumers Goods Firm"""
+
     def __init__(self, simulation, model, agent_number, agent_def):
         super().__init__(simulation, model, agent_number, agent_def)
 
         self.bookkeeper = FirmBookkeeper(self)
         self.eq = CGFirmEquations(self.active_scenario)
         self.eq.set_bookkeeper(self.bookkeeper)
-        
+
         self.labor_mkt_name = "Labor_Market"
         self.cg_mkt_name = "CG_Market"
         self.first = True
-     
-
 
     def step(self):
-        """ Consumer Goods Firm Step 
-        """
-   
+        """Consumer Goods Firm Step"""
+
         if self.first:
             self.create_initial_values()
             self.first = False
@@ -76,46 +73,31 @@ class CGFirm(Firm):
         self.select_deposit_bank()
         self.pay_taxes()
 
-
     def create_expectations(self):
-        """Agent Creates Expectations
-        """
-        self.Se_ct = self.eq.zet(self.Se_ct, 
-                                          self.Se_ct_1)
+        """Agent Creates Expectations"""
+        self.Se_ct = self.eq.zet(self.Se_ct, self.Se_ct_1)
         # Expected sales
         self.Se_ct_1 = self.Se_ct
 
     def compute_desired_output(self):
-        """ Firms compute desired input levels 
-        """
-        self.yd_ct.c_quantity = self.eq.ydt(self.Se_ct, 
-                                           self.inv_ct_1.c_quantity
-                                           )
-        
+        """Firms compute desired input levels"""
+        self.yd_ct.c_quantity = self.eq.ydt(self.Se_ct, self.inv_ct_1.c_quantity)
+
         self.yd_ct_q = self.yd_ct.c_quantity
 
-
-
     def compute_capacity_utilization(self):
-        """CG Firm computes capacity utilization
-        """
+        """CG Firm computes capacity utilization"""
 
-        self.ud_ct = self.eq.udct(self.yd_ct.c_quantity,
-                                self.K_ct.c_quantity
-                               )
-    
- 
-
+        self.ud_ct = self.eq.udct(self.yd_ct.c_quantity, self.K_ct.c_quantity)
 
     def compute_labor_demand(self):
-        """Consumer good firm computes labor demand
-        """
+        """Consumer good firm computes labor demand"""
 
         self.lay_off_from_turnover()
 
-        self.Ndc_t_1 = self.Ndc_t_1    
+        self.Ndc_t_1 = self.Ndc_t_1
         self.Ndc_t = self.eq.ndct(self.K_ct.c_quantity, self.ud_ct)
-  
+
         self.delta_N_ct = self.Ndc_t - self.Ndc_t_1
 
         if self.delta_N_ct > 0:
@@ -125,23 +107,21 @@ class CGFirm(Firm):
             self.lay_off(self.delta_N_ct)
 
     def lay_off(self, delta_N_ct):
-        """ Lay of the employees"""
-        self.bookkeeper.lay_off(delta_N_ct) 
+        """Lay of the employees"""
+        self.bookkeeper.lay_off(delta_N_ct)
 
     def lay_off_from_turnover(self):
-        """ Lay of the employees"""
-        self.bookkeeper.lay_off_from_turnover(self.eq.upsilon)    
-
-        
+        """Lay of the employees"""
+        self.bookkeeper.lay_off_from_turnover(self.eq.upsilon)
 
     def demand_labor(self):
         """
         Sets the labor demand for the firm in the labor market.
 
-        This method sets the labor demand for the firm in the 
+        This method sets the labor demand for the firm in the
         specified labor market.
-        
-        It calls the `set_demand` method of the labor market 
+
+        It calls the `set_demand` method of the labor market
         object to update the demand.
 
         Parameters:
@@ -151,59 +131,50 @@ class CGFirm(Firm):
         - None
         """
 
-
         self.get_a_space(self.labor_mkt_name).set_demand(self, self.labor_demand)
 
-
     def set_output_price(self):
-        """ Firm sets output price for product """
+        """Firm sets output price for product"""
 
         ## Compute Markup
 
         self.mu_ct_1 = self.mu_ct
 
-        self.mu_ct = self.eq.muxt(self.mu_ct_1, 
-                                  self.inv_ct_1.c_quantity, 
-                                  self.S_ct.c_quantity
-                                  )
+        self.mu_ct = self.eq.muxt(
+            self.mu_ct_1, self.inv_ct_1.c_quantity, self.S_ct.c_quantity
+        )
 
+        ## Compute price
+        self.y_ct.c_price = self.eq.pt(
+            self.mu_ct,
+            self.We_ct,
+            self.Ndc_t,
+            self.y_ct.c_quantity + 1,  # Cannot be zero???
+        )
 
-        ## Compute price 
-        self.y_ct.c_price =self.eq.pt(self.mu_ct,
-                                      self.We_ct,
-                                      self.Ndc_t,
-                                      self.y_ct.c_quantity+1  # Cannot be zero???
-                                     )
-        
-    def compute_total_costs(self): # aqui
+    def compute_total_costs(self):  # aqui
         """CG Firms compute the total costs:
-            The costs include:
-                - Labor Costs
-                - Cost on Loans
-                - Capital Costs
+        The costs include:
+            - Labor Costs
+            - Cost on Loans
+            - Capital Costs
         """
-
 
         self.C_ct = self.eq.C_ct()
 
-
-
     def compute_rate_of_capacity_growth(self):
-        """CG Firms compute their rate of capacity growth 
-        """
+        """CG Firms compute their rate of capacity growth"""
 
     def compute_demand_of_K_goods(self):
-        """ With the expected rate of capacity growth CG firms
-            Compute their demand for K goods 
+        """With the expected rate of capacity growth CG firms
+        Compute their demand for K goods
         """
 
     def choose_K_supplier(self):
-        """ CG firms choose their capital supplier in K market 
-        """
+        """CG firms choose their capital supplier in K market"""
 
     def buy_K_goods(self):
-        """ CG Firms buy capital goods
-        """
+        """CG Firms buy capital goods"""
 
     def create_initial_production(self, quantity, price):
         """Firm creates intitial production of goods
@@ -216,21 +187,18 @@ class CGFirm(Firm):
             ConsumptionGood: A consumer Good Stock
         """
 
-        self.y_ct =  ConsumptionGood(c_quantity=quantity,
-                            c_price=price,
-                            c_owner=self,
-                            c_producer=self)
+        self.y_ct = ConsumptionGood(
+            c_quantity=quantity, c_price=price, c_owner=self, c_producer=self
+        )
         return self.y_ct
-    
 
     def produce(self):
         """CG firm produce consumption goods"""
         # NOTE: This method is just to test the market. Needs developing
-    
-        self.y_ct.c_quantity=self.yd_ct.c_quantity
-        self.y_ct.c_price=self.yd_ct.c_price
+
+        self.y_ct.c_quantity = self.yd_ct.c_quantity
+        self.y_ct.c_price = self.yd_ct.c_price
         self.update_inventory(self.y_ct)
-        
 
     def offer_goods(self):
         """
@@ -249,15 +217,13 @@ class CGFirm(Firm):
         space = self.get_a_space(self.cg_mkt_name)
         self.bookkeeper.set_offer(space, self.inv_ct)
 
-
     def update_inventory(self, y):
 
         self.inv_ct_1.c_quantity = self.inv_ct.c_quantity
 
-        self.inv_ct.c_price = (self.inv_ct.c_price + y.c_price)/2
+        self.inv_ct.c_price = (self.inv_ct.c_price + y.c_price) / 2
         self.inv_ct.c_quantity = self.inv_ct.c_quantity + y.c_quantity
 
-    
     def pay_wages(self):
 
         self.bookkeeper.pay_wages()
@@ -267,10 +233,6 @@ class CGFirm(Firm):
 
         self.N_ct = self.bookkeeper.workforce_size()
 
-
-
-
-    
     def create_initial_values(self):
         """
         Creates the initial values for various attributes of the CGFirm class.
@@ -291,60 +253,59 @@ class CGFirm(Firm):
         TODO: This will be changed to calibration in initialization.
         """
         # Inventory (inv_ct)
-        initial_inventory_qnt = rnd.randint(10,500)
-        initial_inventory_price = rnd.randint(1,5)
-        self.inv_ct= self.create_initial_inventory(initial_inventory_qnt,
-                                            initial_inventory_price)
+        initial_inventory_qnt = rnd.randint(10, 500)
+        initial_inventory_price = rnd.randint(1, 5)
+        self.inv_ct = self.create_initial_inventory(
+            initial_inventory_qnt, initial_inventory_price
+        )
 
         # Inventory t-1 (inv_ct_1)
-        initial_inventory_t_1_qnt = rnd.randint(10,500)
-        initial_inventory_t_1_price = rnd.randint(1,5)
-        self.inv_ct_1= self.create_initial_inventory(initial_inventory_t_1_qnt,
-                                            initial_inventory_t_1_price)
-
+        initial_inventory_t_1_qnt = rnd.randint(10, 500)
+        initial_inventory_t_1_price = rnd.randint(1, 5)
+        self.inv_ct_1 = self.create_initial_inventory(
+            initial_inventory_t_1_qnt, initial_inventory_t_1_price
+        )
 
         # Desired output (yd_ct)
-        initial_desired_production_qnt = rnd.randint(70,100)
-        initial_desired_production_price = rnd.randint(1,5)
-        self.yd_ct = self.create_initial_production(initial_desired_production_qnt,
-                                                  initial_desired_production_price)
-
+        initial_desired_production_qnt = rnd.randint(70, 100)
+        initial_desired_production_price = rnd.randint(1, 5)
+        self.yd_ct = self.create_initial_production(
+            initial_desired_production_qnt, initial_desired_production_price
+        )
 
         # Production (output) (y_ct)
-        initial_production_qnt = rnd.randint(70,100)
-        initial_production_price = rnd.randint(1,5)
-        self.y_ct = self.create_initial_production(initial_production_qnt,
-                                                  initial_production_price)
-        
+        initial_production_qnt = rnd.randint(70, 100)
+        initial_production_price = rnd.randint(1, 5)
+        self.y_ct = self.create_initial_production(
+            initial_production_qnt, initial_production_price
+        )
+
         # Expected Sales
-        self.Se_ct = rnd.randint(70,100)
-        self.Se_ct_1 = rnd.randint(70,100)
-        
+        self.Se_ct = rnd.randint(70, 100)
+        self.Se_ct_1 = rnd.randint(70, 100)
 
         # Capital
-        # Capital Stock need to be  actually a dict. 
+        # Capital Stock need to be  actually a dict.
         # With different machines
         # TODO: Implement capital stock as a dict
-        initial_K_stock_qnt = rnd.randint(2,5)
-        initial_K_stock_price = rnd.randint(2,5)
-        self.K_ct = self.create_initial_K_stock(initial_K_stock_qnt,
-                                             initial_K_stock_price)
-
+        initial_K_stock_qnt = rnd.randint(2, 5)
+        initial_K_stock_price = rnd.randint(2, 5)
+        self.K_ct = self.create_initial_K_stock(
+            initial_K_stock_qnt, initial_K_stock_price
+        )
 
         # Sales
-        initial_sales_qnt = rnd.randint(50,90)
-        initial_sales_price = rnd.randint(1,5)
-        self.S_ct = self.create_initial_sales(initial_sales_qnt, 
-                                             initial_sales_price)
-        
+        initial_sales_qnt = rnd.randint(50, 90)
+        initial_sales_price = rnd.randint(1, 5)
+        self.S_ct = self.create_initial_sales(initial_sales_qnt, initial_sales_price)
+
         ## Generate initial salaries (We_ct)
-#        self.ud_ct = rnd.randint(1,5)
+        #        self.ud_ct = rnd.randint(1,5)
         self.Ndc_t = rnd.randint(10, 50)
         self.Ndc_t_1 = rnd.randint(10, 50)
-        self.labor_demand = self.create_initial_labor_demand(self.Ndc_t, 
-                                                             self.We_ct)
+        self.labor_demand = self.create_initial_labor_demand(self.Ndc_t, self.We_ct)
         self.bookkeeper.include_asset(self.labor_demand)
-   
+
     def create_initial_inventory(self, quantity, price):
         """Firm creates intitial production of goods
 
@@ -356,28 +317,24 @@ class CGFirm(Firm):
             ConsumptionGood: A consumer Good Stock
         """
 
-        return ConsumptionGood(c_quantity=quantity,
-                            c_price=price,
-                            c_owner=self,
-                            c_producer=self)
-    
+        return ConsumptionGood(
+            c_quantity=quantity, c_price=price, c_owner=self, c_producer=self
+        )
 
     def create_initial_K_stock(self, quantity, price):
-            """Firm creates intitial Capital Stock
+        """Firm creates intitial Capital Stock
 
-            Args:
-                quantity (number): Initial quantity
-                price (number): Initial price
+        Args:
+            quantity (number): Initial quantity
+            price (number): Initial price
 
-            Returns:
-                CapitalGood: A Capital Goods Stock
-            """
+        Returns:
+            CapitalGood: A Capital Goods Stock
+        """
 
-            return CapitalGood(c_quantity=quantity,
-                                c_price=price,
-                                c_owner=self,
-                                c_producer=None)
-    
+        return CapitalGood(
+            c_quantity=quantity, c_price=price, c_owner=self, c_producer=None
+        )
 
     def create_initial_sales(self, quantity, price):
         """Firm creates intitial sales of goods
@@ -390,18 +347,10 @@ class CGFirm(Firm):
             ConsumptionGood: A consumption Good (sold)
         """
 
-        return ConsumptionGood(c_quantity=quantity,
-                            c_price=price,
-                            c_owner=self,
-                            c_producer=self)
-    
+        return ConsumptionGood(
+            c_quantity=quantity, c_price=price, c_owner=self, c_producer=self
+        )
 
     def create_initial_labor_demand(self, Ndc_t, We_ct):
 
-        return Labor(c_quantity = Ndc_t,
-                     c_price=We_ct,
-                     c_owner=self,
-                     c_producer=self)
-
-
-       
+        return Labor(c_quantity=Ndc_t, c_price=We_ct, c_owner=self, c_producer=self)
--- examples/benchmark/agents/equations.py
+++ examples/benchmark/agents/equations.py
@@ -19,19 +19,16 @@ Todo:
 import numpy as np
 
 
+class Equations:
+    """The equations class for the benchmark model implementation"""
 
-class Equations():
-    """ The equations class for the benchmark model implementation"""
     def __init__(self, active_scenario):
         self.active_scenario = active_scenario
 
-
         self.get_constants()
 
-
     def get_constants(self):
-        """Get the constants of the model
-        """
+        """Get the constants of the model"""
 
         # lambda - prevision error ajustment
         self.expect_lambda = self.active_scenario.expect_lambda
@@ -59,8 +56,6 @@ class Equations():
 
         self.bookkeeper = bookkeeper
 
-
-
     def zet(self, zt, zet_1):
         """Compute expectations in t
 
@@ -72,8 +67,7 @@ class Equations():
             number: expectations in T
         """
 
-        return zet_1 + self.expect_lambda*(zt - zet_1) 
-
+        return zet_1 + self.expect_lambda * (zt - zet_1)
 
     def ydt(self, se_ct, inv_t_1):
         """Compute production in T
@@ -88,18 +82,13 @@ class Equations():
         if inv_t_1 > se_ct:
             self.yd_t = 0
         else:
-            self.yd_t =  se_ct * (1 + self.nu) - inv_t_1
-    
+            self.yd_t = se_ct * (1 + self.nu) - inv_t_1
+
         return self.yd_t
-    
- 
-
-
 
     def uvc(self, We_xt, Nd_xt, yd_xt):
 
-        return (We_xt * Nd_xt)/yd_xt
-    
+        return (We_xt * Nd_xt) / yd_xt
 
     def pt(self, mu_xt, We_xt, Nd_xt, yd_xt):
         """Calculates production prices
@@ -116,10 +105,8 @@ class Equations():
         if yd_xt == 0:
             self.p_t = 1
         else:
-            self.p_t =  (1 + mu_xt)*(We_xt * Nd_xt)/yd_xt
+            self.p_t = (1 + mu_xt) * (We_xt * Nd_xt) / yd_xt
         return self.p_t
-
-    
 
     def muxt(self, mu_xt, inv_t_1, s_et):
         """Updates mark-up
@@ -135,16 +122,16 @@ class Equations():
         """
 
         FN_mu = np.random.lognormal(1.0, 0.03)
-        if inv_t_1/s_et <= self.nu:
+        if inv_t_1 / s_et <= self.nu:
             mu_t = mu_xt + (1 + FN_mu)
         else:
             mu_t = mu_xt + (1 - FN_mu)
-        
+
         if mu_t < 0:
             return mu_xt
         else:
             return mu_t
-        
+
     def sales(self, s_ct):
         """Calculate sales value
 
@@ -156,29 +143,23 @@ class Equations():
         """
 
         return s_ct.c_quantity * s_ct.c_price
-    
 
     def interest(self, i_d):
 
         return i_d.c_quantity * i_d.c_price
-    
 
     def inventory(self, inv):
 
         return inv.c_quantity * inv.c_price
-    
 
     def revenue(self, s_ct, i_d, inv):
-        
+
         sales = self.sales(s_ct)
         interest = self.interest
         inv_costs = self.inventory()
 
         return sales + interest + inv_costs
 
-
-    
- 
 
 class CGFirmEquations(Equations):
     """Consumer Goods Firm specific equations
@@ -187,12 +168,8 @@ class CGFirmEquations(Equations):
         Equations (Object): Specific equations for consumer goods firm
     """
 
-
     def __init__(self, active_scenario):
         super().__init__(active_scenario)
-
-
-
 
     def udct(self, yd_ct, kc_t):
         """Calculates rate of utilization of capital stock
@@ -205,8 +182,7 @@ class CGFirmEquations(Equations):
             float: rate of utilization (0 < udt <= 1)
         """
 
-        return min(1, yd_ct/(kc_t*self.mu_k))
-    
+        return min(1, yd_ct / (kc_t * self.mu_k))
 
     def ndct(self, k_ct, ud_ct):
         """Calculates the labor demmand for CG firms
@@ -221,24 +197,21 @@ class CGFirmEquations(Equations):
         """
         if k_ct == 0:
             ndct = 0
-        else:    
-            ndct = ud_ct*(k_ct/self.l_k)
+        else:
+            ndct = ud_ct * (k_ct / self.l_k)
         return ndct
-    
-    def C_ct(self):  # aqui 
+
+    def C_ct(self):  # aqui
         """Compute total Costs (CGFirms)"""
 
-        return  self.W_ct() + self.Il_ct() + self.Ck_ct()
-        
-
+        return self.W_ct() + self.Il_ct() + self.Ck_ct()
 
     def W_ct(self):
         """Compute Labor Costs"""
-        
+
         W_ct = self.bookkeeper.labor_costs()
 
         return W_ct
-
 
     def Il_ct(self):
         """Compute Loans costs"""
@@ -252,7 +225,7 @@ class CGFirmEquations(Equations):
         """Sales Revenue"""
 
         return self.S_ct() + self.Id_ct() + self.Inv_ct()
-    
+
     def S_ct(self):
         """Sales Revenue"""
         pass
@@ -266,13 +239,6 @@ class CGFirmEquations(Equations):
         pass
 
 
-
-
-        
-
-
-    
-
 class KGFirmEquations(Equations):
     """Capital goods firm specific equations
 
@@ -282,8 +248,6 @@ class KGFirmEquations(Equations):
 
     def __init__(self, active_scenario):
         super().__init__(active_scenario)
-
-
 
     def ndkt(self, y_c):
         """Calculates Labor demand for capital firms
@@ -295,10 +259,8 @@ class KGFirmEquations(Equations):
             ndkt: Labor demand for capital firms
         """
 
-        return y_c/self.mu_n
-    
-        
-    
+        return y_c / self.mu_n
+
 
 class HHEquations(Equations):
     """Household  equations
@@ -307,36 +269,25 @@ class HHEquations(Equations):
         Equations (_type_): _description_
     """
 
-
     def __init__(self, active_scenario):
         super().__init__(active_scenario)
 
-
-
-
     def wd_ht(self, wd_ht_1, u_ht_n):
 
-
-
         FN_w = np.random.lognormal(1.0, 0.03)
-        
+
         if FN_w > 1:
             FN_w = FN_w - 1
 
         ## The step needs to be very low
-        FN_w = FN_w/200
-      
+        FN_w = FN_w / 200
 
         if u_ht_n > 1:
-            self.wdht = wd_ht_1*(1 - FN_w)
+            self.wdht = wd_ht_1 * (1 - FN_w)
         else:
-            self.wdht = wd_ht_1*(1 + FN_w)
+            self.wdht = wd_ht_1 * (1 + FN_w)
 
         if self.wdht <= 0:
-            self.wdht = 0.1    
+            self.wdht = 0.1
 
         return self.wdht
-
-
-        
-
